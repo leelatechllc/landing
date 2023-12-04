be95dac4ce14e9f27f49045dcce5f86304d8e62e
root <root@CIRIACOSSL-BastionDevelopment.ciriacossl.com>
2023-12-04 17:39:14 +0000

    Initial commit Mon Dec  4 17:39:14 UTC 2023

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Simulate loading time
-    setTimeout(function () {
-        document.querySelector('.loader-wrapper').style.display = 'none';
-        document.querySelector('.content').style.display = 'block';
-    }, 3000); // Adjust the time as needed
-});
